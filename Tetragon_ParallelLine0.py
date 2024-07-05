5118434470705c5cae8fd73c6001b450a24329fe
lin213056 <lin213056@163.com>
2024-07-05 21:27:28 +0800

    更改了变量名

--- Tetragon_ParallelLine0.py
+++ Tetragon_ParallelLine0.py
@@ -10,20 +10,15 @@ import numpy as np
 [u2, v2, w2] = [1,2,0]
 coordinate_abcd = np.array([[x1, y1, z1],[x2, y2, z2],[x3, y3, z3],[x4, y4, z4]])
 coordinate_ef = np.array([[u1, v1, w1],[u2, v2, w2]])
-coordinate_name = ['a','b','c','d']
+points_name = ['a', 'b', 'c', 'd']
 
-#计算向量ab,ac,ad,ef
-def vector_computation(x,y):
-    result = x - y
-    return result
+def vector_AtoB(A, B):
+    AB = np.array(B) - np.array(A)
+    return AB
 
-def vector_computation_name(x,y):
-    result = np.array(y)-np.array(x)
-    return result
-
-def boole_computation(x,y):
-    result = np.all(np.cross(x,y) == 0)
-    return result
+def boole_computation(AB, CD):
+    is_cross_zero = np.all(np.cross(AB, CD) == 0)
+    return is_cross_zero
 
 def find_scalar_multiple(A, B):
     k_values = []
@@ -38,46 +33,51 @@ def find_scalar_multiple(A, B):
     else:
         return None
 
-vectors_ax = []
-vectors___ = []
-cross_mults = []
-parallel_edges = {"parallel_to": "ef"}
-boole0 = []
-boole1 = True
-cross_result0 = float(0)
-cross_result1 = []
-cross_result2 = []
-
 """
 计算两组向量：[ab,ac,ad],ef,ae
 """
+vectors_ax = []
+
 for i in range(3):
-    vector_c = np.array(coordinate_abcd[i+1])-np.array(coordinate_abcd[0])
-    vectors_ax.append(vector_c)
+    result_vax = np.array(coordinate_abcd[i + 1]) - np.array(coordinate_abcd[0])
+    vectors_ax.append(result_vax)
 ef = np.array(coordinate_ef[1]) - np.array(coordinate_ef[0])
 
 """
-判断与ef平行的线，写入json
+计算ef与ax的差值
 """
+cross_mults = []
+boole0 = []
+
 for i in range(3):
-    cross_result0 = np.cross(vectors_ax[i],ef)
-    cross_mults.append (cross_result0)
+    result_c = np.cross(vectors_ax[i], ef)
+    cross_mults.append (result_c)
 for i in range(3):
     boole0.append(np.all(cross_mults[i] == 0))
+
+"""
+判断ef与ax是否平行
+平行-->导出这组点与四边形上另外两个点的坐标、名称,写入文件
+"""
+parallel_edges = {"parallel_to": "ef"}
+
 for i in range (3):
     if boole0[i]:
-        serial0 = int((-3/2)*((i+1)**2)+(11/2)*(i+1)-2)
-        serial1 = int((3/2)*((i+1)**2)-(13/2)*(i+1)+8)
+        serial_of_x = int((-3 / 2) * ((i + 1) ** 2) + (11 / 2) * (i + 1) - 2)
+        serial_of_x_ = int((3 / 2) * ((i + 1) ** 2) - (13 / 2) * (i + 1) + 8)
+
         a = coordinate_abcd[0].tolist()
         a_ = coordinate_abcd[i+1].tolist()
-        x = coordinate_abcd[serial0].tolist()
-        x_ = coordinate_abcd[serial1].tolist()
-        aname = coordinate_name[0]
-        a_name = coordinate_name[i+1]
-        xname = coordinate_name[serial0]
-        x_name = coordinate_name[serial1]
-        parallel_edges[f"a{coordinate_name[i+1]}"] = [a,a_]
-        parallel_edges[f"{coordinate_name[serial0]}{coordinate_name[serial1]}"] = [x,x_]
+        x = coordinate_abcd[serial_of_x].tolist()
+        x_ = coordinate_abcd[serial_of_x_].tolist()
+
+        aname = points_name[0]
+        a_name = points_name[i + 1]
+        xname = points_name[serial_of_x]
+        x_name = points_name[serial_of_x_]
+
+        parallel_edges[f"a{points_name[i + 1]}"] = [a, a_]
+        parallel_edges[f"{points_name[serial_of_x]}{points_name[serial_of_x_]}"] = [x, x_]
 
 with open('data.json', 'w') as json_file:
     json.dump(parallel_edges, json_file)
@@ -85,12 +85,12 @@ with open('data.json', 'w') as json_file:
 """
 判断e与ax,ax_,a_x,a_x_的关系
 """
-ax = vector_computation_name(a,x)
-ax_ = vector_computation_name(a,x_)
-a_x = vector_computation_name(a_,x)
-a_x_ = vector_computation_name(a_,x_)
-ae = vector_computation_name(a,coordinate_ef[0])
-a_e = vector_computation_name(a_,coordinate_ef[0])
+ax = vector_AtoB(a, x)
+ax_ = vector_AtoB(a, x_)
+a_x = vector_AtoB(a_, x)
+a_x_ = vector_AtoB(a_, x_)
+ae = vector_AtoB(a, coordinate_ef[0])
+a_e = vector_AtoB(a_, coordinate_ef[0])
 
 if not (boole_computation(ae,ax) and boole_computation(ae,ax_)):
     if not (boole_computation(a_e,a_x)):
